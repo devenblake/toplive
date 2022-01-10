663366243ad0e3b36743f2f8c960e331292ab049
Deven Blake <trinity@trinity.moe>
2022-01-10 18:07:50 -0500

    generator now works

--- generator.py
+++ generator.py
@@ -1,7 +1,33 @@
+#!/usr/bin/env python3
+import argparse
+import collections
 import datetime
 import json
 import sys
 
+def apply_macros(m, t):
+	for key in m.keys():
+		t = t.replace("!%s!" % key, m[key])
+	return t
+
+# print an error to stderr and then exit 1
+def error(s):
+	sys.stderr.write("%s: %s\n" % (sys.argv[0], s))
+	sys.exit(1)
+
+# safely read a file
+# n
+#	file name
+def fileread(n):
+	try:
+		f = open(n)
+	except FileNotFoundError as e:
+		error(e)
+	r = f.read()
+	f.close()
+	return r
+
+# generates macro content
 # data
 #	the full JSON object
 # selection
@@ -14,13 +40,22 @@ def macros(data, selection):
 	year = selection[0]
 	month = selection[1]
 	day = selection[2]
-	macros = {
-		"DY": year,
-		"DM": month,
-		"DD": day,
-		"Dp": datetime.date(int(year), int(month), int(day)).strftime("%d %B %Y"),
-		"Venue": data["concerts"][year][month][day]["venue"]
-	}
+	try:
+		concert = data["concerts"][year][month][day]
+	except KeyError as e:
+		error("macros(): KeyError: %s" % e)
+
+	# trivial date macros to start out
+	macros = collections.OrderedDict([
+		("DY", year),
+		("DM", month),
+		("DD", day),
+		("Dp", datetime.date(int(year), int(month), int(day)).strftime("%d %B %Y"))
+	])
+
+	macros["Venue"] = concert["venue"]
+
+	# video embed
 	video = data["concerts"][year][month][day]["video"]
 	if(video["type"] == "iframe"):
 		macros["Embed"] = '''\
@@ -37,23 +72,36 @@ WIDTH="640"
 		error("What video type is %s? (%s)"
 			% (video["type"], ('data["concerts"]["%s"]["%s"]["%s"]["video"]["type"]'
 				% (year, month, day))))
-	macros["setlist"] = ""
-	for s in data["concerts"][year][month][day]["setlist"]:
-		macros["setlist"] += "<LI>%s</LI>\n" % s
+
+	# setlist ordered list
+	setlist = "<OL>\n"
+	for s in concert["setlist"]:
+		setlist += "<LI>%s</LI>\n" % s
+	setlist += "</OL>\n"
+	macros["Setlist"] = setlist
+
 	return macros
 
-def error(s):
-	sys.stderr.write("%s\n" % s)
-	sys.exit(1)
+def main(argc, argv):
+	parser = argparse.ArgumentParser(add_help=False)
+	parser.add_argument('year')
+	parser.add_argument('month')
+	parser.add_argument('day')
+	args = vars(parser.parse_args(argv[1:]))
+	year, month, day = args["year"], args["month"], args["day"]
 
-with open('data.json') as f:
 	try:
-		data = json.load(f)
+		data = json.loads(fileread("data.json"))
 	except json.decoder.JSONDecodeError as e:
 		error("Invalid JSON file. (%s)" % e)
 
-with open('template-page.html') as f:
-	try:
-		template = f.read()
-	except:
-		error("Error reading template.")
+	template = fileread("template-page.html")
+
+	m = macros(data, [year, month, day])
+	template = apply_macros(m, template)
+	print(end=template)
+
+	return 0;
+
+if __name__ == "__main__":
+	sys.exit(main(len(sys.argv), sys.argv))
